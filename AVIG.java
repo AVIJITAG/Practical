cd1634c4244e690ba2f17b7476b28930fa26df2d
AVIJIT GARAI <110466132+AVIJITAG@users.noreply.github.com>
2022-08-10 15:39:52 +0530

    Add files via upload

---
+++ AVIG.java
@@ -0,0 +1,14 @@
+class AVIG{
+    public static void main(String []args){
+        int sideLength;
+           sideLength=5;
+        int area= sideLength*sideLength;
+        int perimeter = 4*sideLength;
+
+        System.out.println("area of square = "  +area);
+
+        System.out.println("perimeter of the square = "  +perimeter);
+
+
+    }
+}
