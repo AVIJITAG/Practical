11f0fb7c6f37b79007e5a96523494773150f824d
AVIJIT GARAI <110466132+AVIJITAG@users.noreply.github.com>
2022-08-25 12:04:59 +0530

    Add files via upload

---
+++ Javaclass.java
@@ -0,0 +1,12 @@
+// PRINT THE NUMBER FROM 0 TO 10 -
+
+package Javaclass;
+
+public class Javaclass {
+
+	public static void main(String[] args) {
+	    
+		for( int i = 0 ; i < 11; i++ ){			
+		System.out.print(i+ " " );
+	
+		}}}
